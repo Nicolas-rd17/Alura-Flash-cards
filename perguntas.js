a6eb9403ecbc7d701f62f6218afb0f3a67c6d88a
Nicolas-rd17 <168427710+Nicolas-rd17@users.noreply.github.com>
2024-11-28 11:37:51 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,4 @@
+
 criaCartao(
     'Programação',
     'O que é Python?',
@@ -17,22 +18,7 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'Quem é considerado o pai da programação?',
-    'Alan Turing foi um matemático e criptógrafo inglês considerado atualmente como o pai da computação, uma vez que, por meio de suas ideias, foi possível desenvolver o que chamamos hoje de computador.
-'
-)
-
-criaCartao(
-    'Historia',
-    'Qual a data que começou a segunda guerra mundial?',
-    '1 de setembro de 1939
-'
-)
-
-criaCartao(
-    'Matemática',
-    'Qual a area da matemática que estuda as formas?',
-    'Geometria
-'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
